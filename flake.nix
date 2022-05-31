fb15235903d50ef46935090ec24eef579103a451
Hongri Cui <hongri.cui@gmail.com>
2022-05-31 21:41:18 +0900

    Reset configurations

--- flake.nix
+++ flake.nix
@@ -1,22 +1,32 @@
 {
-  description = "A Home Manager flake";
+  description = "Home Manager configuration of sun";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # Specify the source of Home Manager and Nixpkgs
     home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
-    homeConfigurations = {
-      sun = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/sun";
-        username = "sun";
-        configuration.imports = [ ./home.nix ];
-        stateVersion = "21.11";
+  outputs = { home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      username = "sun";
+    in
+    {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        # Specify the path to your home configuration here
+        configuration = import ./home.nix;
+
+        inherit system username;
+        homeDirectory = "/home/${username}";
+        # Update the state version as needed.
+        # See the changelog here:
+        # https://nix-community.github.io/home-manager/release-notes.html#sec-release-21.05
+        stateVersion = "22.05";
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
       };
     };
-    defaultPackage.x86_64-linux = inputs.self.homeConfigurations.sun.activationPackage;
-  };
 }
--- home.nix
+++ home.nix
@@ -14,28 +14,28 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.11";
+  home.stateVersion = "22.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.fish = {
-    enable = true;
-    plugins = [
-      {
-        name="foreign-env";
-        src = pkgs.fetchFromGitHub {
-          owner = "oh-my-fish";
-          repo = "plugin-foreign-env";
-          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
-          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
-        };
-      }
-    ];
-    shellInit = ''
-      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-      end
-    '';
-  };
+  # programs.fish = {
+  #   enable = true;
+  #   plugins = [
+  #     {
+  #       name="foreign-env";
+  #       src = pkgs.fetchFromGitHub {
+  #         owner = "oh-my-fish";
+  #         repo = "plugin-foreign-env";
+  #         rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+  #         sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+  #       };
+  #     }
+  #   ];
+  #   shellInit = ''
+  #     if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  #       fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  #     end
+  #   '';
+  # };
 }
