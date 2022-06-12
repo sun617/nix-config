09e728d78a5092f62494cff911956e7f8a98cb42
Hongri Cui <hongri.cui@gmail.com>
2022-06-13 00:48:51 +0900

    sway settings

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the
@@ -115,6 +115,131 @@
         '';
       }
     ];
+  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    config = {
+      bars = [{
+        fonts = {
+          names = [ "DejaVu Sans Mono" "Font Awesome 6 Free" ];
+          size = 12.0;
+        };
+        position = "top";
+        statusCommand = "i3status-rs ~/.config/i3status-rs/config.toml";
+      }];
+      input = {
+        "type:keyboard" = {
+          xkb_options = "caps:escape,altwin:prtsc_rwin";
+        };
+        "type:touchpad" = {
+          dwt = "enabled";
+          middle_emulation = "enabled";
+          natural_scroll = "enabled";
+          tap = "enabled";
+        };
+      };
+      keybindings =
+        let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in
+        lib.mkOptionDefault {
+          # take screenshot
+          "Control+Mod1+a" = "exec grim -g '$(slurp)' - | wl-copy --type image/png && wl-paste > $(xdg-user-dir PICTURES)/$(date +'%Y%m%d_%H%M%S_grim.png')";
+          "Control+Mod1+s" = "exec grim -o $(swaymsg -t get_outputs | jq --raw-output '.[] | select(.focused) | .name') - | wl-copy --type image/png && wl-paste > $(xdg-user-dir PICTURES)/$(date +'%Y%m%d_%H%M%S_grim.png')";
+          # recording
+          "Control+Mod1+r" = "exec wf-recorder -o $(swaymsg -t get_outputs | jq --raw-output '.[] | select(.focused) | .name') -c h264_vaapi -d /dev/dri/renderD128 -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y%m%d_%H%M%S.mp4')";
+          "Control+Mod1+BackSpace" = "exec killall -s SIGINT wf-recorder";
+          # lockscreen
+          "Control+${modifier}+l" = "exec swaylock -eFki /usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png";
+          # Switch application
+          "${modifier}+a" = "[app_id='pavucontrol'] focus";
+          "${modifier}+c" = "[class='Google-chrome'] focus";
+          "${modifier}+d" = "[app_id='DBeaver'] focus";
+          "${modifier}+f" = "[app_id='firefox'] focus";
+          "${modifier}+p" = "[class='Postman'] focus";
+          "${modifier}+s" = "[class='Slack'] focus";
+          "${modifier}+t" = "[app_id='foot'] focus";
+
+          "Control+${modifier}+a" = "focus parent";
+          "Control+${modifier}+f" = "fullscreen";
+          "Control+${modifier}+v" = "splith";
+          "Control+${modifier}+s" = "splitv";
+          # Switch the current container between different layout styles
+          "Control+${modifier}+w" = "layout stacking";
+          "Control+${modifier}+t" = "layout tabbed";
+          "Control+${modifier}+e" = "layout toggle split";
+          # Toggle the current focus between tiling and floating mode
+          "${modifier}+Mod1+m" = "floating toggle";
+          # Swap focus between the tiling area and the floating area
+          "Control+${modifier}+m" = "focus mode_toggle";
+
+          # Hot keys
+          "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume 0 +5%";
+          "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume 0 -5%";
+          "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute 0 toggle";
+          "XF86MonBrightnessDown" = "exec --no-startup-id light -U 10";
+          "XF86MonBrightnessUp" = "exec --no-startup-id light -A 10";
+          "XF86Sleep" = "exec systemctl suspend";
+          "XF86AudioPlay" = "exec playerctl play-pause";
+          "XF86AudioStop" = "exec playerctl stop";
+          "XF86AudioPrev" = "exec playerctl previous";
+          "XF86AudioNext" = "exec playerctl next";
+        };
+      menu = "wofi --insensitive --show drun";
+      modifier = "Mod4";
+      output = { "*".bg = "/usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png fill"; };
+      startup = [
+        { command = "udiskie --tray"; }
+        { command = "fcitx5 -d"; }
+        { command = "mako"; }
+        { command = "swayrd"; }
+        { command = "foot --server"; }
+        { command = "pkill kanshi; exec kanshi"; always = true; }
+        {
+          command = ''
+            swayidle -w \
+                    timeout 900  "swaylock -efFki /usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png" \
+                    timeout 1800  "swaymsg 'output * dpms off'" \
+                          resume "swaymsg 'output * dpms on'" \
+                    timeout 7200 "systemctl suspend"
+          '';
+        }
+      ];
+      terminal = "footclient";
+      window = {
+        commands = [
+          {
+            command = "inhibit_idle fullscreen";
+            criteria.app_id = "firefox";
+          }
+          {
+            command = "floating enable";
+            criteria = {
+              app_id = "firefox";
+              title = "Picture-in-Picture";
+            };
+          }
+          {
+            command = "inhibit_idle fullscreen";
+            criteria.class = "Google-chrome";
+          }
+          {
+            # File upload etc.
+            command = "floating enable";
+            criteria.app_id = "xdg-desktop-portal-gtk";
+          }
+          {
+            command = "floating enable";
+            criteria.class = "fcitx5-config-qt";
+          }
+          {
+            command = "floating enable";
+            criteria.app_id = "pavucontrol";
+          }
+        ];
+      };
+    };
   };
 
   # programs.fish = {
