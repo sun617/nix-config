9adb536ee75116bd2fc53016cf244a174213bc51
Hongri Cui <hongri.cui@gmail.com>
2022-06-14 23:54:21 +0900

    Add i3status-rust

--- home.nix
+++ home.nix
@@ -133,7 +133,7 @@
           size = 12.0;
         };
         position = "top";
-        statusCommand = "i3status-rs ~/.config/i3status-rs/config.toml";
+        statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
       }];
       input = {
         "type:keyboard" = {
@@ -247,6 +247,50 @@
             criteria.app_id = "pavucontrol";
           }
         ];
+      };
+    };
+  };
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      top = {
+        blocks = [
+          {
+            block = "backlight";
+            device = "intel_backlight";
+          }
+          {
+            block = "sound";
+            step_width = 3;
+          }
+          {
+            block = "networkmanager";
+            primary_only = true;
+          }
+          {
+            block = "battery";
+          }
+          {
+            block = "disk_space";
+          }
+          {
+            block = "memory";
+            format_mem = "{mem_used}/{mem_total}({mem_used_percents})";
+            format_swap = "{swap_used}/{swap_total}({swap_used_percents})";
+          }
+          {
+            block = "cpu";
+          }
+          {
+            block = "load";
+          }
+          {
+            block = "time";
+          }
+        ];
+        icons = "awesome5";
+        theme = "solarized-dark";
       };
     };
   };
