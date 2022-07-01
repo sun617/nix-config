f70e403e5939c5393d4f0f4c8a506116fe7a299c
Hongri Cui <hongri.cui@gmail.com>
2022-07-01 20:56:10 +0900

    Setup swapfile

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -26,7 +26,12 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 1024 * 16;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
