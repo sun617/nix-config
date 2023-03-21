e86104f42d23871bdefb70013bb8a9d53f9547c5
Hongri Cui <hongri.cui@gmail.com>
2023-03-21 20:58:15 +0900

    Add node template

---
+++ templates/node/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Node development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = with pkgs; mkShellNoCC {
+        packages = [
+          nodejs-slim-16_x
+          nodePackages.npm
+        ];
+      };
+    };
+}
