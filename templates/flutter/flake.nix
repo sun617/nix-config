ab0f86fb90677855ef685ec67129910ab40e6fc5
Hongri Cui <hongri.cui@gmail.com>
2023-06-10 15:44:06 +0900

    Update flutter flake

--- templates/flutter/flake.nix
+++ templates/flutter/flake.nix
@@ -8,13 +8,27 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          android_sdk.accept_license = true;
+        };
+      };
+      android = pkgs.androidenv.composeAndroidPackages {
+        platformVersions = [ "30" "31" "32" "33" ];
+      };
     in
     {
       devShells.${system}.default = with pkgs; mkShellNoCC {
         packages = [
           flutter
+          jdk
+          android.platform-tools
         ];
+
+        ANDROID_SDK_ROOT = "${android.androidsdk}/libexec/android-sdk";
+        JAVA_HOME = pkgs.jdk;
       };
     };
 }
