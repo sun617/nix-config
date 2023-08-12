4557a065eb400fd9593cb8618e64db036fae7cef
Hongri Cui <hongri.cui@gmail.com>
2023-08-12 16:32:03 +0900

    Install vscode

--- home-manager/wayland/vscode.nix
+++ home-manager/wayland/vscode.nix
@@ -1,14 +1,5 @@
-{ pkgs, ... }:
-
 {
   programs.vscode = {
     enable = true;
-    package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
-      src = (builtins.fetchTarball {
-        url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-        sha256 = "0nb7iavknf4rpdc7fi54x0hnhp4acgfb4yrf0xl4h5h599y651q3";
-      });
-      version = "latest";
-    });
   };
 }
