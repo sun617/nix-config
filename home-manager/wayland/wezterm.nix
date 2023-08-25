7ba1c395a22aae365b5bfe27fafcd35230487cf6
Hongri Cui <hongri.cui@gmail.com>
2023-08-25 17:02:02 +0900

    disable alt-enter key binding for wezterm

--- home-manager/wayland/wezterm.nix
+++ home-manager/wayland/wezterm.nix
@@ -4,7 +4,7 @@
     extraConfig = ''
       local act = wezterm.action
 
-      wezterm.on("update-right-status", function(window, pane)
+      wezterm.on('update-right-status', function(window, pane)
         window:set_right_status(window:active_workspace())
       end)
 
@@ -14,34 +14,36 @@
           'JetBrains Mono',
           'Noto Sans Mono CJK JP',
         }),
-        color_scheme = "Solarized Dark Higher Contrast",
+        color_scheme = 'Solarized Dark Higher Contrast',
         use_fancy_tab_bar = false,
         tab_bar_at_bottom = true,
         tab_max_width = 80,
         -- key bindings
-        leader = { key=";", mods="CTRL", timeout_milliseconds=2000 },
+        leader = { key = ';', mods = 'CTRL', timeout_milliseconds = 2000 },
         keys = {
+          -- disable default assignment
+          { key = 'Enter', mods = 'ALT', action = act.DisableDefaultAssignment },
           -- copy mode
-          {key="/", mods="LEADER", action=act.Search("CurrentSelectionOrEmptyString")},
-          {key="c", mods="LEADER", action=act.ActivateCopyMode},
-          {key="q", mods="LEADER", action=act.QuickSelect},
+          { key = '/', mods = 'LEADER', action = act.Search('CurrentSelectionOrEmptyString') },
+          { key = 'c', mods = 'LEADER', action = act.ActivateCopyMode },
+          { key = 'q', mods = 'LEADER', action = act.QuickSelect },
           -- pane
-          {key="s", mods="LEADER", action=act.SplitVertical{domain="CurrentPaneDomain"}},
-          {key="v", mods="LEADER", action=act.SplitHorizontal{domain="CurrentPaneDomain"}},
-          {key="h", mods="LEADER", action=act.ActivatePaneDirection("Left")},
-          {key="l", mods="LEADER", action=act.ActivatePaneDirection("Right")},
-          {key="k", mods="LEADER", action=act.ActivatePaneDirection("Up")},
-          {key="j", mods="LEADER", action=act.ActivatePaneDirection("Down")},
-          {key="z", mods="LEADER", action=act.TogglePaneZoomState},
+          { key = 's', mods = 'LEADER', action = act.SplitVertical{ domain = 'CurrentPaneDomain' } },
+          { key = 'v', mods = 'LEADER', action = act.SplitHorizontal{domain = 'CurrentPaneDomain' } },
+          { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
+          { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection('Right') },
+          { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection('Up') },
+          { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection('Down') },
+          { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
           -- tab
-          {key="n", mods="LEADER", action=act.SpawnTab("CurrentPaneDomain")},
-          {key="j", mods="LEADER|CTRL", action=act.ActivateTabRelative(-1)},
-          {key="k", mods="LEADER|CTRL", action=act.ActivateTabRelative(1)},
+          { key = 'n', mods = 'LEADER', action = act.SpawnTab('CurrentPaneDomain') },
+          { key = 'j', mods = 'LEADER|CTRL', action = act.ActivateTabRelative(-1) },
+          { key = 'k', mods = 'LEADER|CTRL', action = act.ActivateTabRelative(1) },
           -- workspace
-          {key="N", mods="LEADER", action=act.SwitchToWorkspace},
-          {key=" ", mods="LEADER", action=act.ShowLauncherArgs{flags="FUZZY|WORKSPACES"}},
-          {key="K", mods="LEADER", action=act.SwitchWorkspaceRelative(1)},
-          {key="J", mods="LEADER", action=act.SwitchWorkspaceRelative(-1)},
+          { key = 'N', mods = 'LEADER', action = act.SwitchToWorkspace },
+          { key = ' ', mods = 'LEADER', action = act.ShowLauncherArgs{ flags = 'FUZZY|WORKSPACES' } },
+          { key = 'K', mods = 'LEADER', action = act.SwitchWorkspaceRelative(1) },
+          { key = 'J', mods = 'LEADER', action = act.SwitchWorkspaceRelative(-1) },
         },
         enable_wayland = true
       }
