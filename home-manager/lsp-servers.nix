df16d7cc944eb0e9a0f66ea0da9c23f1b5a5638a
Hongri Cui <hongri.cui@gmail.com>
2023-10-05 10:15:05 +0900

    Add @tailwindcss/language-server

--- home-manager/lsp-servers.nix
+++ home-manager/lsp-servers.nix
@@ -5,9 +5,11 @@
     gopls
     lua-language-server
     nil
+    nodePackages."@tailwindcss/language-server"
+    # https://github.com/NixOS/nixpkgs/pull/259042
+    # nodePackages."@vue/language-server"
     nodePackages.prettier
     nodePackages.typescript-language-server
-    # nodePackages.volar
     nodePackages.vscode-langservers-extracted
     nodePackages.yaml-language-server
     rust-analyzer
