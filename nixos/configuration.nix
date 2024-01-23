8a2512f55fc0601c6f5499d74cb0bd53f9314926
Hongri Cui <hongri.cui@gmail.com>
2024-01-24 00:14:24 +0900

    extract fonts and icons configurations

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,8 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      # Include fonts and icons configurations
+      ./fonts-and-icons.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -189,44 +191,4 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
 
-  # fonts
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-emoji
-    font-awesome_6
-    (nerdfonts.override { fonts = [ "Hack" ]; })
-  ];
-  # icons
-  environment.systemPackages = let
-    mkDefaultCursorIcon = pkgs.writeTextDir "share/icons/default/index.theme" ''
-      [icon theme]
-      Name=Default
-      Comment=Default Cursor Theme
-      Inherits=Bibata-Modern-Ice
-    '';
-  in [
-    pkgs.bibata-cursors
-    mkDefaultCursorIcon
-  ];
-
-  # https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1326957279
-  system.fsPackages = [ pkgs.bindfs ];
-  fileSystems = let
-    mkRoSymBind = path: {
-      device = path;
-      fsType = "fuse.bindfs";
-      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-    };
-    aggregatedFonts = pkgs.buildEnv {
-      name = "system-fonts";
-      paths = config.fonts.packages;
-      pathsToLink = [ "/share/fonts" ];
-    };
-  in {
-    # Create an FHS mount to support flatpak host icons/fonts
-    "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
-    "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
-  };
 }
