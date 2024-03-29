ed4429c208022f6eb1386dd60eccaf5592b6c111
Hongri Cui <hongri.cui@gmail.com>
2024-03-29 15:36:05 +0900

    Remove fonts and icons settings for flatpak applications

--- nixos/fonts-and-icons.nix
+++ nixos/fonts-and-icons.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # fonts
@@ -23,23 +23,4 @@
     pkgs.bibata-cursors
     mkDefaultCursorIcon
   ];
-
-  # https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1326957279
-  system.fsPackages = [ pkgs.bindfs ];
-  fileSystems = let
-    mkRoSymBind = path: {
-      device = path;
-      fsType = "fuse.bindfs";
-      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-    };
-    aggregatedFonts = pkgs.buildEnv {
-      name = "system-fonts";
-      paths = config.fonts.packages;
-      pathsToLink = [ "/share/fonts" ];
-    };
-  in {
-    # Create an FHS mount to support flatpak host icons/fonts
-    "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
-    "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
-  };
 }
