2789e6573917511709690567955177435f4685cc
Hongri Cui <hongri.cui@gmail.com>
2024-03-31 21:14:43 +0900

    Revert "Remove templates"
    This reverts commit 29ad60986d601cdbdaf393ad23b0599a59abf8b4.

---
+++ templates/README.md
@@ -0,0 +1,10 @@
+```
+# Go
+nix flake init -t "github:sun617/nix-config#go"
+# Node
+nix flake init -t "github:sun617/nix-config#node"
+# Rust
+nix flake init -t "github:sun617/nix-config#node"
+# Flutter
+nix flake init -t "github:sun617/nix-config#flutter"
+```
---
+++ templates/rust/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = with pkgs; mkShellNoCC {
+        packages = [
+          gcc
+          rustup
+        ];
+      };
+    };
+}
