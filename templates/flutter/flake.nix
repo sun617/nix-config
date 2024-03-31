c0de8764255ae85193d23b01bc540112cb6024dc
Hongri Cui <hongri.cui@gmail.com>
2024-03-31 22:24:28 +0900

    Update flutter settings

--- templates/flutter/flake.nix
+++ templates/flutter/flake.nix
@@ -15,9 +15,21 @@
           android_sdk.accept_license = true;
         };
       };
+      # https://github.com/NixOS/nixpkgs/issues/289936
+      buildToolsVersionForAapt2 = "34.0.0-rc4";
       androidComposition = pkgs.androidenv.composeAndroidPackages {
-        buildToolsVersions = [ "30.0.3" ];
-        platformVersions = [ "33" ];
+        buildToolsVersions = [buildToolsVersionForAapt2 "30.0.3"];
+        platformVersions = [ "34" "33" ];
+        extraLicenses = [
+          "android-googletv-license"
+          "android-sdk-arm-dbt-license"
+          # "android-sdk-license"
+          "android-sdk-preview-license"
+          "google-gdk-license"
+          # "intel-android-extra-license"
+          # "intel-android-sysimage-license"
+          "mips-android-sysimage-license"
+        ];
       };
       androidSdk = androidComposition.androidsdk;
     in
@@ -30,6 +42,7 @@
         ];
 
         ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+        GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/${buildToolsVersionForAapt2}/aapt2";
       };
     };
 }
