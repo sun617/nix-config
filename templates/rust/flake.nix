73e8d36619295b05ce9a983a8667eb30e43d82df
Hongri Cui <hongri.cui@gmail.com>
2024-06-02 11:32:05 +0900

    Update rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -4,22 +4,34 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
     # Flake Utils
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, fenix, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+        fenixStable = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-std"
+          "rustc"
+          "rustfmt"
+        ];
       in
       {
         devShells.default = with pkgs; mkShellNoCC {
-          buildInputs = [
-            gcc
-            rustup
+          nativeBuildInputs = [
+            fenixStable
+            # gcc
+            # pkg-config
+            # openssl
             rust-analyzer
           ];
         };
