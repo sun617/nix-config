1bdc88120a8f43b710f4647f952270be71321fe7
Hongri Cui <hongri.cui@gmail.com>
2025-05-25 16:09:32 +0900

    Update cloudflared configuration: restructure tunnel credentials path and replace the old credentials file with a new encrypted file specific to the tunnel ID.

--- hosts/aix1pro/nixos/cloudflared.nix
+++ hosts/aix1pro/nixos/cloudflared.nix
@@ -5,15 +5,15 @@
     enable = true;
     tunnels = {
       "65404191-9a66-450f-91f2-aaffe497d14f" = {
-        credentialsFile = config.sops.secrets."cloudflared/tunnel-credentials".path;
+        credentialsFile = config.sops.secrets."cloudflared/tunnel-credentials/65404191-9a66-450f-91f2-aaffe497d14f".path;
         default = "http_status:404";
       };
     };
   };
 
   sops.secrets = {
-    "cloudflared/tunnel-credentials" = {
-      sopsFile = ../../../secrets/system/cloudflared/tunnel-credentials.enc;
+    "cloudflared/tunnel-credentials/65404191-9a66-450f-91f2-aaffe497d14f" = {
+      sopsFile = ../../../secrets/system/cloudflared/tunnel-credentials/65404191-9a66-450f-91f2-aaffe497d14f.enc;
       format = "binary";
     };
   };
